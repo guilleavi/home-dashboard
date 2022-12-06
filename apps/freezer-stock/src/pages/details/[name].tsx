f9cafa0660d618bf2c2b6e111b5a147926118a19
Guillermina Avigliano <guillerminaavigliano@yahoo.com.ar>
2022-12-06 18:22:57 -0300

    details refactor

--- apps/freezer-stock/src/pages/details/[name].tsx
+++ apps/freezer-stock/src/pages/details/[name].tsx
@@ -6,14 +6,48 @@ import type {
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
 } from "next/types"
+import Spinner from "@components/Spinner/Spinner"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { ProductDetails } from "@custom-types/product"
+import { getProductDetails, getAllProductDetails } from "@services/products"
 
 const DetailsPage = ({
   name,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const title = `${toPascalCase(name)} Stock Details`
+
+  const [instances, setInstances] = useState<Array<ProductDetails>>([])
+  const [showSpinner, setShowSpinner] = useState(false)
+
+  useEffect(() => {
+    setShowSpinner(true)
+
+    if (name === "all") {
+      const fetchAllDetails = async () => {
+        setInstances(await getAllProductDetails())
+      }
+
+      fetchAllDetails()
+    } else {
+      const fetchDetails = async (productName: string) => {
+        setInstances(await getProductDetails(productName))
+      }
+
+      fetchDetails(name)
+    }
+
+    setShowSpinner(false)
+  }, [name])
+
   return (
     <PageContainer htmlTitle={`Freezer stock - ${title}`} pageTitle={title}>
-      <StockDetails name={name} />
+      <Spinner show={showSpinner}>
+        <StockDetails instances={instances} />
+      </Spinner>
+      <Link href="/">
+        <button className="main-button">Back</button>
+      </Link>
     </PageContainer>
   )
 }
