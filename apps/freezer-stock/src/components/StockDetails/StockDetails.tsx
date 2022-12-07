4dee6ea3171b2dfb2a3f105c93d86c3e0bb8acf6
Guillermina Avigliano <guillerminaavigliano@yahoo.com.ar>
2022-12-07 14:51:21 -0300

    add filter and sort to details grid

--- apps/freezer-stock/src/components/StockDetails/StockDetails.tsx
+++ apps/freezer-stock/src/components/StockDetails/StockDetails.tsx
@@ -1,4 +1,5 @@
 import type { ProductDetails } from "@custom-types/product"
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid"
 import { trimDateString } from "@utils/date"
 import styles from "./StockDetails.module.scss"
 
@@ -6,38 +7,31 @@ type StockDetailsProps = {
   instances: Array<ProductDetails>
 }
 
-// TODO: spinner not working
+const columns: GridColDef[] = [
+  { field: "name", headerName: "Name", width: 300 },
+  { field: "units", headerName: "Units", width: 125 },
+  {
+    field: "expirationDate",
+    headerName: "Expiration Date",
+    width: 200,
+    valueGetter: (params: GridValueGetterParams) =>
+      trimDateString(params.row["expirationDate"].toString()),
+  },
+]
+
 const StockDetails = ({ instances }: StockDetailsProps) => {
-  // TODO: add sort and filter
   return (
     <>
       {instances && instances.length ? (
-        <table className={styles["list"]}>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Units</th>
-              <th>Expiration Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {instances.map((instance) => {
-              const formattedDate = trimDateString(
-                instance.expirationDate.toString(),
-              )
-
-              return (
-                <tr key={instance.instanceId}>
-                  <td>{instance.name}</td>
-                  <td>{instance.units}</td>
-                  <td>
-                    <time dateTime={formattedDate}>{formattedDate}</time>
-                  </td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+        <div className={styles["list"]}>
+          <DataGrid
+            getRowId={(row) => row.instanceId}
+            rows={instances}
+            columns={columns}
+            pageSize={8}
+            sortingOrder={["desc", "asc"]}
+          />
+        </div>
       ) : (
         <p>No results found</p>
       )}
