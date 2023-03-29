4a543005bcaf045f6d21526a1a5cd8b781b0d4c0
Guillermina Avigliano <guillerminaavigliano@yahoo.com.ar>
2023-03-29 17:32:36 -0300

    create category routing

--- apps/buy-me-api/index.ts
+++ apps/buy-me-api/index.ts
@@ -1,10 +1,12 @@
 import express, { Express } from 'express'
 import indexRouter from './routes/index.js'
+import categoryRouter from './routes/category/index.js'
 
 const app: Express = express()
 const port = 3000 // TODO: install dotenv to set the port as an env var
 
 app.use('/', indexRouter)
+app.use('/category', categoryRouter)
 
 app.listen(port, () => {
   console.log(`[server]: Server is runnung at http://localhost:${port}`)
