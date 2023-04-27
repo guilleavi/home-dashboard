c7be4e88abc1db975bc0f882601df83594c0dc98
Guillermina Avigliano <guillerminaavigliano@yahoo.com.ar>
2023-04-27 10:30:36 -0300

    update db schema

---
+++ apps/freezer-stock/prisma/migrations/20230427123314_add_storagedate_field/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `ProductInstance` ADD COLUMN `storagenDate` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
--- apps/freezer-stock/src/types/prisma/generated/client/index-browser.js
+++ apps/freezer-stock/src/types/prisma/generated/client/index-browser.js
@@ -1,10 +1,12 @@
-Object.defineProperty(exports, "__esModule", { value: true })
+
+Object.defineProperty(exports, "__esModule", { value: true });
 
 const {
   Decimal,
   objectEnumValues,
-  makeStrictEnum,
-} = require("./runtime/index-browser")
+  makeStrictEnum
+} = require('./runtime/index-browser')
+
 
 const Prisma = {}
 
@@ -12,37 +14,37 @@ exports.Prisma = Prisma
 
 /**
  * Prisma Client JS version: 4.8.1
- * Query Engine version: d6e67a83f971b175a593ccc12e15c4a757f93ffe
+ * Query Engine version: ca7fcef713137fa11029d519a9780db130cca91d
  */
 Prisma.prismaVersion = {
   client: "4.8.1",
-  engine: "d6e67a83f971b175a593ccc12e15c4a757f93ffe",
+  engine: "ca7fcef713137fa11029d519a9780db130cca91d"
 }
 
 Prisma.PrismaClientKnownRequestError = () => {
   throw new Error(`PrismaClientKnownRequestError is unable to be run in the browser.
-In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`)
-}
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)};
 Prisma.PrismaClientUnknownRequestError = () => {
   throw new Error(`PrismaClientUnknownRequestError is unable to be run in the browser.
-In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`)
-}
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
 Prisma.PrismaClientRustPanicError = () => {
   throw new Error(`PrismaClientRustPanicError is unable to be run in the browser.
-In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`)
-}
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
 Prisma.PrismaClientInitializationError = () => {
   throw new Error(`PrismaClientInitializationError is unable to be run in the browser.
-In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`)
-}
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
 Prisma.PrismaClientValidationError = () => {
   throw new Error(`PrismaClientValidationError is unable to be run in the browser.
-In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`)
-}
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
 Prisma.NotFoundError = () => {
   throw new Error(`NotFoundError is unable to be run in the browser.
-In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`)
-}
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
 Prisma.Decimal = Decimal
 
 /**
@@ -50,21 +52,22 @@ Prisma.Decimal = Decimal
  */
 Prisma.sql = () => {
   throw new Error(`sqltag is unable to be run in the browser.
-In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`)
-}
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
 Prisma.empty = () => {
   throw new Error(`empty is unable to be run in the browser.
-In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`)
-}
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
 Prisma.join = () => {
   throw new Error(`join is unable to be run in the browser.
-In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`)
-}
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
 Prisma.raw = () => {
   throw new Error(`raw is unable to be run in the browser.
-In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`)
-}
+In case this error is unexpected for you, please report it in https://github.com/prisma/prisma/issues`,
+)}
 Prisma.validator = () => (val) => val
+
 
 /**
  * Shorthand utilities for JSON filtering
@@ -76,7 +79,7 @@ Prisma.AnyNull = objectEnumValues.instances.AnyNull
 Prisma.NullTypes = {
   DbNull: objectEnumValues.classes.DbNull,
   JsonNull: objectEnumValues.classes.JsonNull,
-  AnyNull: objectEnumValues.classes.AnyNull,
+  AnyNull: objectEnumValues.classes.AnyNull
 }
 
 /**
@@ -84,39 +87,38 @@ Prisma.NullTypes = {
  */
 // Based on
 // https://github.com/microsoft/TypeScript/issues/3192#issuecomment-261720275
-function makeEnum(x) {
-  return x
-}
+function makeEnum(x) { return x; }
 
 exports.Prisma.ProductInstanceScalarFieldEnum = makeEnum({
-  instanceId: "instanceId",
-  name: "name",
-  units: "units",
-  expirationDate: "expirationDate",
-})
+  instanceId: 'instanceId',
+  name: 'name',
+  units: 'units',
+  storageDate: 'storageDate'
+});
 
 exports.Prisma.ProductScalarFieldEnum = makeEnum({
-  productId: "productId",
-  name: "name",
-  monthsToFreeze: "monthsToFreeze",
-})
+  productId: 'productId',
+  name: 'name',
+  monthsToFreeze: 'monthsToFreeze'
+});
 
 exports.Prisma.SortOrder = makeEnum({
-  asc: "asc",
-  desc: "desc",
-})
+  asc: 'asc',
+  desc: 'desc'
+});
 
 exports.Prisma.TransactionIsolationLevel = makeStrictEnum({
-  ReadUncommitted: "ReadUncommitted",
-  ReadCommitted: "ReadCommitted",
-  RepeatableRead: "RepeatableRead",
-  Serializable: "Serializable",
-})
+  ReadUncommitted: 'ReadUncommitted',
+  ReadCommitted: 'ReadCommitted',
+  RepeatableRead: 'RepeatableRead',
+  Serializable: 'Serializable'
+});
+
 
 exports.Prisma.ModelName = makeEnum({
-  Product: "Product",
-  ProductInstance: "ProductInstance",
-})
+  Product: 'Product',
+  ProductInstance: 'ProductInstance'
+});
 
 /**
  * Create the Client
